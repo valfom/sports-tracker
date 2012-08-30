b9e3c391ba1d0e45c0b8743c3ed574ba14b93d43
Valeriy Fomin <valfom.com@gmail.com>
2012-08-30 14:48:48 +0400

    Created simple interface

--- src/com/valfom/tracker/TrackerActivity.java
+++ src/com/valfom/tracker/TrackerActivity.java
@@ -2,12 +2,57 @@ package com.valfom.tracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrackerActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+	private static boolean isPaused = false;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Button startBtn = (Button) findViewById(R.id.startBtn);
+        final Button stopBtn = (Button) findViewById(R.id.stopBtn);
+        final Button pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        
+        stopBtn.setVisibility(View.GONE);
+        pauseBtn.setVisibility(View.GONE);
+        
+        startBtn.setOnClickListener(new View.OnClickListener() {
+        	
+            public void onClick(View v) {
+            	
+            	startBtn.setVisibility(View.GONE);
+            	stopBtn.setVisibility(View.VISIBLE);
+                pauseBtn.setVisibility(View.VISIBLE);
+            }
+        });
+        
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+        	
+            public void onClick(View v) {
+            	
+            	startBtn.setVisibility(View.VISIBLE);
+            	stopBtn.setVisibility(View.GONE);
+                pauseBtn.setVisibility(View.GONE);
+            }
+        });
+        
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+        	
+            public void onClick(View v) {
+            	
+            	if (isPaused)
+            		pauseBtn.setText(R.string.pause_btn);
+            	else
+            		pauseBtn.setText(R.string.start_btn);
+            	
+            	isPaused = !isPaused;
+            }
+        });
     }
 }
