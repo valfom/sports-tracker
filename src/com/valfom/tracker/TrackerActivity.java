6166af1e6061bbb998dd35089739bc89da6bd34a
Valeriy Fomin <valfom.com@gmail.com>
2012-09-01 01:22:23 +0400

    Bugs fixed

--- src/com/valfom/tracker/TrackerActivity.java
+++ src/com/valfom/tracker/TrackerActivity.java
@@ -1,52 +1,74 @@
 package com.valfom.tracker;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Timer;
 import java.util.TimerTask;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.location.Criteria;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class TrackerActivity extends Activity {
-	
-	private static boolean isPaused = false;
-	long startTime = 0;
-	TextView timeTV;
-	Timer timer;
-	long pauseTime = 0;
-	long millis;
 	
 	private static final int MIN_UPDATE_TIME = 1000;
 	private static final int MIN_UPDATE_DISTANCE = 0;
 	
-	private LocationManager locationManager;
-	private Criteria criteria;
+	private static boolean isPaused = false;
 	
-	Location prevLocation = null;
+	private static Timer timer;
+	private static LocationManager locationManager;
+	private static ProgressDialog progressDialog;
 	
-	float maxSpeed = 0;
-	double distance = 0;
-    
-	class secondTask extends TimerTask {
+	// UI
+	private static TextView timeTV;
+	private static Button startBtn;
+	private static Button stopBtn;
+	private static Button pauseBtn;
+	private static TextView distanceTV;
+	private static TextView curSpeedTV;
+	private static TextView maxSpeedTV;
+	
+	private static Location prevLocation;
+	
+	private static float maxSpeed = 0;
+	private static double distance = 0;
+	
+	private static boolean flag = false;
+	
+	/*DEBUG*/
+	private static NumberPicker debugRoundNum;
+	private static Button clearBtn;
+	/*-----*/
+	
+	class TrackerTimer extends TimerTask {
+		
+		private long millis = 0;
+		private long pauseTime = 0;
+		private long startTime = System.currentTimeMillis();
 
-        @Override
+		@Override
         public void run() {
+        	
             TrackerActivity.this.runOnUiThread(new Runnable() {
 
                 public void run() {
-       
                 	
                 	if (!isPaused) {
             
 	                	millis = System.currentTimeMillis() - startTime - pauseTime;
+	                	
 	                	int seconds = (int) (millis / 1000);
 	                	int minutes = seconds / 60;
 	                	seconds     = seconds % 60;
@@ -69,48 +91,69 @@ public class TrackerActivity extends Activity {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        timeTV = (TextView) findViewById(R.id.timeTV);
+        /*DEBUG*/
+        debugRoundNum = (NumberPicker) findViewById(R.id.debugRoundNum);
+        debugRoundNum.setMinValue(0);
+        debugRoundNum.setMaxValue(50);
+        debugRoundNum.setValue(16);
         
-        final Button startBtn = (Button) findViewById(R.id.startBtn);
-        final Button stopBtn = (Button) findViewById(R.id.stopBtn);
-        final Button pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        clearBtn = (Button) findViewById(R.id.clearBtn);
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+        	
+            public void onClick(View v) {
+            
+            	distance = 0;
+            	prevLocation = null;
+            	
+            	distanceTV.setText(R.string.default_value);
+            }
+        });
+        /*-----*/
+        
+        timeTV = (TextView) findViewById(R.id.timeTV);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        stopBtn = (Button) findViewById(R.id.stopBtn);
+        pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        distanceTV = (TextView) findViewById(R.id.distanceTV);
+        curSpeedTV = (TextView) findViewById(R.id.curSpeedTV);
+        maxSpeedTV = (TextView) findViewById(R.id.maxSpeedTV);
         
         stopBtn.setVisibility(View.GONE);
         pauseBtn.setVisibility(View.GONE);
+        
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         
         startBtn.setOnClickListener(new View.OnClickListener() {
         	
             public void onClick(View v) {
             	
-            	startBtn.setVisibility(View.GONE);
-            	stopBtn.setVisibility(View.VISIBLE);
-                pauseBtn.setVisibility(View.VISIBLE);
-                
-                pauseTime = 0;
-                startTime = System.currentTimeMillis();
-                timer = new Timer();
-                timer.schedule(new secondTask(), 0, 1000);
-                
-                registerListener();
+            	if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            	
+            		Intent locationSettingsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);  
+            		startActivity(locationSettingsIntent);
+            	} else {
+	                
+	                progressDialog = ProgressDialog.show(TrackerActivity.this, "", "Starting GPS...");
+	                progressDialog.setCancelable(true);
+	                
+	                progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener(){
+
+	                	  public void onCancel(DialogInterface dialog) {
+	                	   
+	                		  unregisterAllListeners();
+	                	  }
+	               });
+	                
+	               registerListener();	                	          
+            	}
             }
         });
         
         stopBtn.setOnClickListener(new View.OnClickListener() {
         	
             public void onClick(View v) {
-            	
-            	timer.cancel();
-            	
-            	timeTV.setText(R.string.time_default_value);
-            	
-            	isPaused = false;
-            	
-            	maxSpeed = 0;
-            	distance = 0;
-            	
-            	startBtn.setVisibility(View.VISIBLE);
-            	stopBtn.setVisibility(View.GONE);
-                pauseBtn.setVisibility(View.GONE);
+
+            	stop();
                 
                 unregisterAllListeners();
             }
@@ -120,160 +163,152 @@ public class TrackerActivity extends Activity {
         	
             public void onClick(View v) {
             	
-            	if (isPaused)
-            		pauseBtn.setText(R.string.pause_btn);
-            	else
-            		pauseBtn.setText(R.string.start_btn);
-            	
-            	isPaused = !isPaused;
+            	pause();
             }
         });
-        
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) && 
-      		  !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-        
-        	Toast.makeText(getApplicationContext(), "all providers disabled", Toast.LENGTH_SHORT).show();
-        }
-        
-        criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setPowerRequirement(Criteria.POWER_HIGH);
-        criteria.setAltitudeRequired(false);
-        criteria.setBearingRequired(false);
-        criteria.setSpeedRequired(true);
-        criteria.setCostAllowed(true);
+    }
+    
+    private void start() {
+    	
+    	maxSpeed = 0;
+    	distance = 0;
+    	
+    	prevLocation = null;
+    	
+    	isPaused = false;
+    	
+    	startBtn.setVisibility(View.GONE);
+    	stopBtn.setVisibility(View.VISIBLE);
+        pauseBtn.setVisibility(View.VISIBLE);
+
+        timer = new Timer();
+        timer.schedule(new TrackerTimer(), 0, 1000);
+    }
+    
+    private void stop() {
+    	
+    	flag = false;
+    	
+    	timer.cancel();
+    	
+    	timeTV.setText(R.string.time_default_value);
+    	distanceTV.setText(R.string.default_value);
+    	curSpeedTV.setText(R.string.default_value);
+    	maxSpeedTV.setText(R.string.default_value);
+    	
+    	startBtn.setVisibility(View.VISIBLE);
+    	stopBtn.setVisibility(View.GONE);
+        pauseBtn.setVisibility(View.GONE);
+    }
+    
+    private void pause() {
+    	
+    	if (isPaused)
+    		pauseBtn.setText(R.string.pause_btn);
+    	else
+    		pauseBtn.setText(R.string.resume_btn);
+    	
+    	isPaused = !isPaused;
     }
     
     private void updateWithNewLocation(Location location) {
 	    
-		if (location != null) {
+		if (location != null && !isPaused) {
 			
-//			Toast.makeText(getApplicationContext(), "location", Toast.LENGTH_SHORT).show();
+			if (!flag) {
+			
+				flag = true;
+				
+				progressDialog.dismiss();
+				
+				start();
+			}
 	     
-			Float speed = location.getSpeed();
+			float speed = location.getSpeed();
+			
+			curSpeedTV.setText(String.format("%.1f", speed));
 			
 			if (speed > maxSpeed) {
 				
 				maxSpeed = speed;
 				
-				TextView maxSpeedTV = (TextView) findViewById(R.id.maxSpeedTV);
 				maxSpeedTV.setText(String.format("%.1f", speed));
 			}
 			
 			if (prevLocation != null) {
 				
-				Double lat1 = prevLocation.getLatitude();
-				Double lng1 = prevLocation.getLongitude();
+				double lat1 = round(prevLocation.getLatitude(), debugRoundNum.getValue());
+				double lng1 = round(prevLocation.getLongitude(), debugRoundNum.getValue());
 				
-				Double lat2 = location.getLatitude();
-				Double lng2 = location.getLongitude();
+				double lat2 = round(location.getLatitude(), debugRoundNum.getValue());
+				double lng2 = round(location.getLongitude(), debugRoundNum.getValue());
 				
-				distance += distance(lat1, lng1, lat2, lng2);
+				distance += calculateDistance(lat1, lng1, lat2, lng2);
 				
-				TextView distanceTV = (TextView) findViewById(R.id.distanceTV);
 				distanceTV.setText(String.format("%.2f", distance));
 				
-//				Toast.makeText(getApplicationContext(), String.valueOf(lat2) + " " + String.valueOf(lng2), Toast.LENGTH_SHORT).show();
+//				Log.d("DIST", lat1 + " " + lng1 + " - " + lat2 + " " + lng2 + " = " + calculateDistance(lat1, lng1, lat2, lng2));
 			}
 			
 			prevLocation = location;
-			
-			TextView curSpeedTV = (TextView) findViewById(R.id.curSpeedTV);
-			curSpeedTV.setText(String.format("%.1f", speed));
 		}
 	}
-	
-	private LocationListener bestProviderListener = new LocationListener() {
-	
+    
+    @Override
+	protected void onDestroy() {
+    
+    	super.onDestroy();
+    	unregisterAllListeners();
+	}
+
+	public double round(double d, int p) {
+
+    	return new BigDecimal(d).setScale(p, RoundingMode.HALF_UP).doubleValue();
+	}
+    
+    private LocationListener gpsProviderListener = new LocationListener() {
+    	
 		public void onLocationChanged(Location location) {
 			
 			updateWithNewLocation(location);
 		}
+		
 		public void onProviderDisabled(String provider) {}
+		
 		public void onProviderEnabled(String provider) {
 	
 			registerListener();
 		}
-		public void onStatusChanged(String provider,int status, Bundle extras) {}
-	};
-	
-	private LocationListener bestAvailableProviderListener = new LocationListener() {
-	
-		public void onProviderEnabled(String provider) {}
 		
-		public void onProviderDisabled(String provider) {
-	
-			registerListener();
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			
+			Log.d("DIST", "CHANGED");
 		}
-	
-		public void onLocationChanged(Location location) {
-
-			updateWithNewLocation(location);
-		}
-		
-		public void onStatusChanged(String provider,int status, Bundle extras) {}
 	};
-	
+    
+    private void unregisterAllListeners() {
+    		
+		locationManager.removeUpdates(gpsProviderListener);
+	}
+    	
 	private void registerListener() {
 		
 		unregisterAllListeners();
 		
-		String bestProvider = locationManager.getBestProvider(criteria, false);
-		String bestAvailableProvider = locationManager.getBestProvider(criteria, true);
-		
-		if (bestProvider == null) {
-			
-		} else if (bestProvider.equals(bestAvailableProvider)) {
-			
-			locationManager.requestLocationUpdates(bestAvailableProvider, MIN_UPDATE_TIME, MIN_UPDATE_DISTANCE, bestAvailableProviderListener);
-		} else {
-		
-			locationManager.requestLocationUpdates(bestProvider, MIN_UPDATE_TIME, MIN_UPDATE_DISTANCE, bestProviderListener);
-		
-			if (bestAvailableProvider != null) {
-				locationManager.requestLocationUpdates(bestAvailableProvider, MIN_UPDATE_TIME, MIN_UPDATE_DISTANCE, bestAvailableProviderListener);
-			} else {
-		
-				List<String> allProviders = locationManager.getAllProviders();
-				
-				for (String provider : allProviders)
-					locationManager.requestLocationUpdates(provider, 0, 0,bestProviderListener);
-			}
-		}
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_UPDATE_TIME, MIN_UPDATE_DISTANCE, gpsProviderListener);		
 	}
 	
-	private void unregisterAllListeners() {
+	private double calculateDistance(double llat1, double llong1, double llat2, double llong2) {
 		
-		locationManager.removeUpdates(bestProviderListener);
-		locationManager.removeUpdates(bestAvailableProviderListener);
-	}
-
-	private double distance(double llat1, double llong1, double llat2, double llong2) {
+		// http://gis-lab.info/qa/great-circles.html
 		
-//		радиус сферы (Земли)
 		int rad = 6372795;
 
-//		координаты двух точек
-//		double llong1 = 37.497074;
-//		double llat1 = 55.618672;
-//
-//		double llong2 = 37.502776;
-//		double llat2 = 55.619391;
-		
-//		37.499665,55.618978 200
-//		37.497074,55.618672 370
-		
-//		37.502776,55.619391
+		double lat1 = llat1 * Math.PI / 180;
+		double lat2 = llat2 * Math.PI / 180;
+		double long1 = llong1 * Math.PI / 180;
+		double long2 = llong2 * Math.PI / 180;
 
-//		в радианах
-		double lat1 = llat1*Math.PI/180;
-		double lat2 = llat2*Math.PI/180;
-		double long1 = llong1*Math.PI/180;
-		double long2 = llong2*Math.PI/180;
-
-//		косинусы и синусы широт и разницы долгот
 		double cl1 = Math.cos(lat1);
 		double cl2 = Math.cos(lat2);
 		double sl1 = Math.sin(lat1);
@@ -282,11 +317,10 @@ public class TrackerActivity extends Activity {
 		double cdelta = Math.cos(delta);
 		double sdelta = Math.sin(delta);
 
-//		вычисления длины большого круга
-		double y = Math.sqrt(Math.pow(cl2*sdelta,2)+Math.pow(cl1*sl2-sl1*cl2*cdelta,2));
-		double x = sl1*sl2+cl1*cl2*cdelta;
-		double ad = Math.atan2(y,x);
-		double dist = ad*rad;
+		double y = Math.sqrt(Math.pow(cl2 * sdelta, 2) + Math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2));
+		double x = sl1 * sl2 + cl1 * cl2 * cdelta;
+		double ad = Math.atan2(y, x);
+		double dist = ad * rad;
 		
 //		Toast.makeText(getApplicationContext(), String.valueOf(dist), Toast.LENGTH_SHORT).show();
 
