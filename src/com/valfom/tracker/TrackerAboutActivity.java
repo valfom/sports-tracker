c8f021c8d67fdddea270f085bd19c5bbc6966ccf
Valeriy Fomin <valfom.com@gmail.com>
2012-11-15 17:09:10 +0400

    Added Splash Screen

--- src/com/valfom/tracker/TrackerAboutActivity.java
+++ src/com/valfom/tracker/TrackerAboutActivity.java
@@ -1,8 +1,5 @@
 package com.valfom.tracker;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.ActionBar;
