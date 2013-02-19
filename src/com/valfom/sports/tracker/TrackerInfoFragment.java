628785948d025e39f006191f55648b3f147511b3
Valeriy Fomin <valfom.com@gmail.com>
2013-02-19 18:55:00 +0400

    New Info UI

--- src/com/valfom/sports/tracker/TrackerInfoFragment.java
+++ src/com/valfom/sports/tracker/TrackerInfoFragment.java
@@ -24,10 +24,17 @@ public class TrackerInfoFragment extends SherlockFragment {
 		
 		View v = getView();
 		
-		TextView tvDate = (TextView) v.findViewById(R.id.dateTV);
-		TextView tvDistance = (TextView) v.findViewById(R.id.distTV);
-		TextView tvDuration = (TextView) v.findViewById(R.id.timeTV);
-		TextView tvMaxSpeed = (TextView) v.findViewById(R.id.maxSpeedTV);
+		TextView tvDate = (TextView) v.findViewById(R.id.tvDateInfo);
+		TextView tvDistance = (TextView) v.findViewById(R.id.tvDistanceInfo);
+		TextView tvDuration = (TextView) v.findViewById(R.id.tvDurationInfo);
+		TextView tvMaxSpeed = (TextView) v.findViewById(R.id.tvMaxSpeedInfo);
+		TextView tvAvgSpeed = (TextView) v.findViewById(R.id.tvAvgSpeedInfo);
+		TextView tvMaxPace = (TextView) v.findViewById(R.id.tvMaxPaceInfo);
+		TextView tvAvgPace = (TextView) v.findViewById(R.id.tvAvgPaceInfo);
+//		TextView tvMaxAltitude = (TextView) v.findViewById(R.id.tvMaxAltitudeInfo);
+//		TextView tvMinAltitude = (TextView) v.findViewById(R.id.tvMinAltitudeInfo);
+		TextView tvGainAltitude = (TextView) v.findViewById(R.id.tvAltitudeGainInfo);
+		TextView tvLossAltitude = (TextView) v.findViewById(R.id.tvAltitudeLossInfo);
 		
 		TextView tvActivityInfo = (TextView) v.findViewById(R.id.tvActivityInfo);
 		
@@ -57,11 +64,52 @@ public class TrackerInfoFragment extends SherlockFragment {
 	        
 	        TrackerSettings settings = new TrackerSettings(getActivity());
 	        
-	        tvDate.setText(track.getDate());
-	        tvDistance.setText(String.valueOf(settings.convertDistance(track.getDistance())));
-	        tvDuration.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
-	        tvMaxSpeed.setText(String.valueOf(settings.convertSpeed(track.getMaxSpeed())));
+	        String date = track.getDate();
+			double distance = track.getDistance();
+			float maxSpeed = track.getMaxSpeed();
+			float avgSpeed = (float) track.getAvgSpeed();
+			double maxPace = (float) track.getMaxPace();
+			double avgPace = (float) track.getAvgPace();
+			double lossAltitude = (float) track.getAltitudeLoss();
+			double gainAltitude = (float) track.getAltitudeGain();
 	        
+			distance = settings.convertDistance(distance);
+	    	maxSpeed = settings.convertSpeed(maxSpeed);
+	    	avgSpeed = settings.convertSpeed(avgSpeed);
+			
+	        long millisAvgPace = (long) avgPace;
+			int secondsAvgPace = (int) (millisAvgPace / 1000);
+	    	int minutesAvgPace = secondsAvgPace / 60;
+	    	secondsAvgPace = secondsAvgPace % 60;
+	    	int hoursAvgPace = minutesAvgPace / 60;
+	    	minutesAvgPace = minutesAvgPace % 60;
+	    	
+	    	long millisMaxPace = (long) maxPace;
+			int secondsMaxPace = (int) (millisMaxPace / 1000);
+	    	int minutesMaxPace = secondsMaxPace / 60;
+	    	secondsMaxPace = secondsMaxPace % 60;
+	    	int hoursMaxPace = minutesMaxPace / 60;
+	    	minutesMaxPace = minutesMaxPace % 60;
+	        
+	    	tvDate.setText(date);
+	        tvDuration.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+	        tvDistance.setText(String.format("%.2f", distance));
+	        tvMaxSpeed.setText(String.format("%02.0f", maxSpeed));
+	        tvAvgSpeed.setText(String.format("%02.0f", avgSpeed));
+	
+	        if (hoursMaxPace > 0)
+	        	tvMaxPace.setText(String.format("%02d:%02d:%02d",hoursMaxPace, minutesMaxPace, secondsMaxPace));
+	        else
+	        	tvMaxPace.setText(String.format("%02d:%02d", minutesMaxPace, secondsMaxPace));
+	
+			if (hoursAvgPace > 0)
+				tvAvgPace.setText(String.format("%02d:%02d:%02d",hoursAvgPace, minutesAvgPace, secondsAvgPace));
+			else
+				tvAvgPace.setText(String.format("%02d:%02d", minutesAvgPace, secondsAvgPace));
+	
+			tvLossAltitude.setText(String.format("%02.0f", lossAltitude));
+			tvGainAltitude.setText(String.format("%02.0f", gainAltitude));
+	
 	        String[] activities = v.getResources().getStringArray(R.array.activities_array);
 		    
 	        int activityId = track.getActivity();
